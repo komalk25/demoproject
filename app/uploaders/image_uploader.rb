01f0ab0a7430848f1a0f4dd53760fd5fe632c517
komalk25 <komalk@futurismtechnologies.com>
2022-05-26 23:56:46 +0530

    styling changes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,11 +30,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [220,240]
+    process :resize_to_fit => [200,200]
   end
 
   version :default do
-    process resize_to_fit: [200,240]
+    process resize_to_fit: [200,200]
   end  
 
   # Add an allowlist of extensions which are allowed to be uploaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  
   root 'products#index'
-  get "/products", to: "products#index"
+  
 
 
   devise_for :sellers
