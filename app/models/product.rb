46710bcbba8a49f1e8539254ae0a1d07c77319b1
komalk25 <komalk@futurismtechnologies.com>
2022-05-31 23:29:50 +0530

    add cart model, cartitem model and user model and setup there association and do the styling changes in devise gem form

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,5 +8,7 @@ class Product < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10 }
   validates :price, numericality: {greater_than_or_equal_to: 0}
   validates :image, presence: true
+
+  has_many :cart_items
 end
  
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_112901) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_31_130656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -39,5 +54,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_112901) do
     t.index ["reset_password_token"], name: "index_sellers_on_reset_password_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
   add_foreign_key "products", "sellers"
 end
