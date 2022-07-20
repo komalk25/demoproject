3a2fb636f4637e29e63762e93d5eaae8853255d6
komalk25 <komalk@futurismtechnologies.com>
2022-07-20 22:28:27 +0530

    test changes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,13 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+=begin    
+    
+    #it {should validate_presence_of(:title)}
+    #it {should validate_presence_of(:description)} 
+    
+=end
+
+    let(:seller){
+        Seller.new(:email => "komal@gmail.com",:name => "komal")
+    }
+
     subject{
         described_class.new(
             title: "woment top",
             description: "regular fit, round neck top",
+            price: 345.5,
+            image: "image.jpg",
+            seller_id: 1
 
         )
     }
 
-    it {should validate_presence_of(:title)}
+    
+    it "valid with valid attributes" do
+        expect(subject).to be_valid
+    end
+
+    
+    it "not valid without a title" do
+        subject.title = nil
+        expect(subject).to_not be_valid
+    end
+
+    it "not valid without a description" do
+        subject.description = nil
+        expect(subject).to_not be_valid
+    end
+
+    it "not valid without a price" do
+        subject.price = nil
+        expect(subject).to_not be_valid
+    end    
+
+    describe "Associations" do 
+        it {should belong_to(:seller)}
+    end    
+
 end
